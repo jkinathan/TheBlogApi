323d8577617858d84f5a1859a264113cfae63c5c
jkinobe <jkinobe@gmail.com>
2020-06-18 14:39:23 +0300

    custom permissions for rest_framework API

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
+from .permissions import IsAuthorOrReadOnly # new
 from .serializers import PostSerializer
 from .models import Post
 
@@ -12,5 +13,6 @@ class PostList(generics.ListCreateAPIView):
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     #permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthorOrReadOnly,)#custom permissions for rest_framework API
     queryset = Post.objects.all()
     serializer_class = PostSerializer
