2f4e76adc08e326090e0d69233e0c4585c679496
jkinobe <jkinobe@gmail.com>
2020-06-20 15:06:47 +0300

    Session and Basic Authentication

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -45,7 +45,12 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
     'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': [ # new
+    'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.BasicAuthentication'
+    ],
 }
 
 MIDDLEWARE = [
