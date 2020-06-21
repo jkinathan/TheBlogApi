da53fb0c47178ff2309e797cdf3d0d8c9786849c
jkinobe <jkinobe@gmail.com>
2020-06-21 14:31:05 +0300

    refactoring to viewsets and routers serializers class for model

--- blog_project/posts/serializers.py
+++ blog_project/posts/serializers.py
@@ -1,8 +1,14 @@
 #posts/serializers.py 
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth import get_user_model # new
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('id','title','author','body','created_at')
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id','username',)
